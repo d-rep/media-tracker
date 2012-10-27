725cc17c2aa20c31a27d2ab3588d3a4b839a580d
Drew Repasky <drew.repasky@gmail.com>
2012-10-27 13:43:32 -0400

    Updated eclipse metadata so that the projects use m2eclipse, and added core dependencies like Struts2, Spring3, slf4j, & commons-lang.

--- media-tracker-domain/src/main/java/com/repaskys/web/domain/Media.java
+++ media-tracker-domain/src/main/java/com/repaskys/web/domain/Media.java
@@ -1,6 +1,14 @@
 package com.repaskys.web.domain;
 
-public class Media {
+import java.io.Serializable;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+
+public class Media implements Serializable {
+	private static final long serialVersionUID = -2708426606290701594L;
 	private String title;
 
 	public String getTitle() {
@@ -9,5 +17,21 @@ public class Media {
 
 	public void setTitle(String title) {
 		this.title = title;
+	}
+	
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return EqualsBuilder.reflectionEquals(this, obj);
+	}
+
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this,
+				ToStringStyle.MULTI_LINE_STYLE);
 	}
 }
